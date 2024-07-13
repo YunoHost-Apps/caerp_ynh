14c1976dd1f5297439c3624907bd6ff2cb2583db
Félix Piédallu <felix@piedallu.me>
2024-07-13 14:08:17 +0200

    Coding style, install nodejs at start of script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,58 +4,53 @@
 # COMMON VARIABLES
 #=================================================
 
+nodejs_version=16
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
 
-__ynh_endi_patch_src() {
+_ynh_endi_patch_src() {
     # Patching requirements.txt to use system provided Cython
     cython_version=$(cython3 --version 2>&1 | sed 's|Cython version ||')
     sed -i "s|Cython=.*|Cython==$cython_version|" "$install_dir/endi/requirements.txt"
 }
 
-__ynh_endi_build() {
-    #=================================================
-    # NPM setup
-    #=================================================
+
+_ynh_endi_build_ui() {
     ynh_use_nodejs
-    ynh_install_nodejs --nodejs_version=16
 
     pushd "$install_dir/endi" 2>&1
-        ynh_exec_as $app $ynh_node_load_PATH $ynh_npm --prefix js_sources install 2>&1
-        ynh_exec_as $app $ynh_node_load_PATH $ynh_npm --prefix vue_sources install 2>&1
+        ynh_exec_as "$app" "$ynh_node_load_PATH" "$ynh_npm" --prefix js_sources install 2>&1
+        ynh_exec_as "$app" "$ynh_node_load_PATH" "$ynh_npm" --prefix vue_sources install 2>&1
 
-        ynh_script_progression --message="Building Web interface code..." --weight=1
-        ynh_exec_as $app $ynh_node_load_PATH make prodjs devjs prodjs2 devjs2 2>&1 \
+        ynh_exec_as "$app" "$ynh_node_load_PATH" make prodjs devjs prodjs2 devjs2 2>&1 \
             || ynh_die --message="Build of javascript code failed, maybe because of high RAM usage!"
     popd 2>&1
+}
 
-    #=================================================
-    # Python + Virtualenv setup
-    #=================================================
-    ynh_script_progression --message="Installing Python dependencies and Endi..." --weight=1
-
-    __ynh_python_venv_setup --venv_dir="$install_dir/venv"
-    python_venv_site_packages=$(__ynh_python_venv_get_site_packages_dir -d "$install_dir/venv")
-    chown -R $app:www-data "$install_dir/venv"
+_ynh_endi_build_python
+    _ynh_python_venv_setup --venv_dir="$install_dir/venv"
+    python_venv_site_packages=$(_ynh_python_venv_get_site_packages_dir -d "$install_dir/venv")
+    chown -R "$app:www-data" "$install_dir/venv"
 
     pushd "$install_dir/endi" 2>&1
-        ynh_exec_as $app "$install_dir/venv/bin/python3" ./setup.py install 2>&1
+        ynh_exec_as "$app" "$install_dir/venv/bin/python3" ./setup.py install 2>&1
     popd 2>&1
 
     chmod 750 "$install_dir"
     chmod -R o-rwx "$install_dir"
-    chown -R $app:www-data "$install_dir"
+    chown -R "$app:www-data" "$install_dir"
 }
 
-__ynh_endi_migratedb() {
+_ynh_endi_migratedb() {
     pushd "$install_dir" 2>&1
         ynh_exec_as $app "$install_dir/venv/bin/endi-admin" "$install_dir/endi.ini" \
             syncdb
     popd 2>&1
  }
 
-__ynh_endi_add_admin() {
+_ynh_endi_add_admin() {
     pushd "$install_dir" 2>&1
         ynh_exec_as $app "$install_dir/venv/bin/endi-admin" "$install_dir/endi.ini" \
             useradd --group=admin --user="admin" --pwd="$password" --email="admin@$domain"
@@ -67,7 +62,7 @@ __ynh_endi_add_admin() {
 # Python Venv HELPERS
 #=================================================
 
-__ynh_python_venv_setup() {
+_ynh_python_venv_setup() {
     local -A args_array=( [d]=venv_dir= [p]=packages= )
     local venv_dir
     local packages
@@ -81,7 +76,7 @@ __ynh_python_venv_setup() {
     fi
 }
 
-__ynh_python_venv_get_site_packages_dir() {
+_ynh_python_venv_get_site_packages_dir() {
     local -A args_array=( [d]=venv_dir= )
     local venv_dir
     ynh_handle_getopts_args "$@"
